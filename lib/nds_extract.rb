d392d15dc779c08f319946776ed6aeeca7fa60fd
Karl Matthes <karlmatthes@gmail.com>
2019-11-05 22:50:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,11 +54,7 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
-  movie_with_director_array = []
-  movies_collection.length.times do |index|
-    movie_with_director_array.push(movie_with_director_name(name, movies_collection))
-  end
-  return movie_with_director_array
+  nil
 end
 
 
@@ -87,6 +83,9 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  
+  
+  
 end
 
 # ----------------    End of Your Code Region --------------------
@@ -100,4 +99,3 @@ def studios_totals(nds)
 end
 
 
-pp movies_with_director_key(directors_database)
