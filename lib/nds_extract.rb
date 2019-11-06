6601ac1ff32850d7be33b1c4b2ee81dca015a57f
Karl Matthes <karlmatthes@gmail.com>
2019-11-06 00:23:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -83,6 +83,13 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
+  # { :name => "A", :movies => [{ :title => "Test" }] }
+  # becomes... [[{:title => "Test1", :director_name => "A"}],
+  #...          [{:title => "Test2", :director_name => "B"}],
+  #...          [{:title => "Test3", :director_name => "C"}]]
+  
+  
+  
 
 end
 
