f53e907dd4ced344e5a71557968f56ad02c655a3
Karl Matthes <karlmatthes@gmail.com>
2019-11-06 01:41:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -88,7 +88,11 @@ def gross_per_studio(collection)
   studio_gross_hash = {}
   
   collection.length.times do |index|
-    studio_gross_hash[collection[index][:studio]] += collection[index][:worldwide_gross]
+    if !studio_gross_hash[collection[index][:studio]]
+      studio_gross_hash[collection[index][:studio]] += collection[index][:worldwide_gross]
+    else
+      studio_gross_hash[collection[index][:studio]] += collection[index][:worldwide_gross]
+    end
   end
   
   return studio_gross_hash
